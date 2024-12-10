2bea1db1e517bcac3a42e96c93ef2126da1f055a
Przemysław Smażyk <sma.przemek@gmail.com>
2024-12-10 18:09:04 +0100

    deployment fix 2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env[PORT] ? parseInt(process.env[PORT]) : 3001;
+const port = process.env['PORT'] ? parseInt(process.env[PORT]) : 3001;
 const cors = require('cors');
 
 const genericRoutes = require('./src/routes/routes');
